ecb7e73369021073e3e38744fb0509358d8402db
Lester Martin <lester.martin@gmail.com>
2021-03-12 16:47:59 -0500

    getting to an initial state

--- src/main/java/wordcount/WordCountBatch.java
+++ src/main/java/wordcount/WordCountBatch.java
@@ -1,50 +1,64 @@
 package wordcount;
 
-import org.apache.flink.api.common.functions.FilterFunction;
-import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.util.Collector;
 
 public class WordCountBatch
 {
     public static void main(String[] args)
             throws Exception
     {
-        //boiler plate code
+        // boiler plate code
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
         ParameterTool params = ParameterTool.fromArgs(args);
         env.getConfig().setGlobalJobParameters(params);
 
+        // read the input file
+        DataSet<String> lines = env.readTextFile(params.get("input"));
 
-        DataSet<String> text = env.readTextFile(params.get("input"));
+        //calculate word counts
+        DataSet<Tuple2<String, Integer>> wordcounts =
+                // split up the lines in pairs (2-tuples) containing: (word,1)
+                lines.flatMap(new Tokenizer())
+                        // group by the tuple field "0" and sum up tuple field "1"
+                        .groupBy(0).sum(1);
 
-        DataSet<String> filtered = text.filter(new FilterFunction<String>()
-
-        {
-            public boolean filter(String value)
-            {
-                return value.startsWith("N");
-            }
-        });
-        DataSet<Tuple2<String, Integer>> tokenized = filtered.map(new Tokenizer());
-
-        DataSet<Tuple2<String, Integer>> counts = tokenized.groupBy(new int[] { 0 }).sum(1);
+        // output the dataset
         if (params.has("output"))
         {
-            counts.writeAsCsv(params.get("output"), "\n", " ");
-
+            wordcounts.writeAsCsv(params.get("output"), "\n", " ");
+            // the next line triggers the lazy execution to begin
             env.execute("WordCount Batch Example");
         }
     }
 
+    // *************************************************************************
+    //     USER FUNCTIONS
+    // *************************************************************************
+
+    /**
+     * Implements the string tokenizer that splits sentences into words as a user-defined
+     * FlatMapFunction. The function takes a line (String) and splits it into multiple pairs in the
+     * form of "(word,1)" ({@code Tuple2<String, Integer>}).
+     */
     public static final class Tokenizer
-            implements MapFunction<String, Tuple2<String, Integer>>
-    {
-        public Tuple2<String, Integer> map(String value)
-        {
-            return new Tuple2(value, Integer.valueOf(1));
+            implements FlatMapFunction<String, Tuple2<String, Integer>> {
+
+        @Override
+        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+            // normalize and split the line
+            String[] tokens = value.toLowerCase().split("\\W+");
+
+            // emit the pairs
+            for (String token : tokens) {
+                if (token.length() > 0) {
+                    out.collect(new Tuple2<>(token, 1));
+                }
+            }
         }
     }
 }
